7fefd151355ee61420ad4ea621c0f969933f4339
GonzaBillia <gonzalobillia@gmail.com>
2025-02-24 13:56:08 -0300

    quito puerto para axios

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -3,9 +3,8 @@ import axios from 'axios';
 
 const PORT = import.meta.env.VITE_PORT
 const HOST = import.meta.env.VITE_HOST
-console.log(`https://${HOST}:${PORT}`)
 const apiClient = axios.create({
-  baseURL: `https://${HOST}:${PORT}`, // Reemplaza con la URL de tu backend
+  baseURL: `https://${HOST}`, // Reemplaza con la URL de tu backend
   withCredentials: true, // Importante para enviar/recibir cookies
 });
 
