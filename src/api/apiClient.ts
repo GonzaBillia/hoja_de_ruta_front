7b2a65a02701fcb65c25eb834f6d4cccc1127b2e
GonzaBillia <gonzalobillia@gmail.com>
2025-04-09 09:21:21 -0300

    hago unos cambios sobre el uso de .env

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 const HOST = import.meta.env.VITE_BACKEND_HOST
 
 const apiClient = axios.create({
-  baseURL: HOST, // Reemplaza con la URL de tu backend
+  baseURL: `https://${HOST}`, // Reemplaza con la URL de tu backend
   withCredentials: true, // Importante para enviar/recibir cookies
 });
 export default apiClient;
